827297b0527a7cb51e4fc677c9a16b38185ea2a1
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-01-23 12:18:56 -0300

    inicio do projeto, com as rotas

---
+++ src/User/user.cotroller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Get, Param, Patch, Post, Put } from "@nestjs/common";
+
+@Controller("users")
+export class UserController{
+
+    @Get()
+    async list(){
+        return {users:[]}
+    }
+
+    @Get(":id")
+    async readOne(@Param() param){
+        return {user:{}, param}
+    }
+
+    @Post()
+    async create(@Body() body) {
+        return {body};
+    }
+
+    @Put(":id")
+    async update(@Body() body,@Param() params){
+        return {
+            method: "put",
+            body,
+            params
+        }
+    }
+
+    @Patch(":id")
+    async updatePartial(@Body() body, @Param() params){
+        return {
+            method: "patch",
+            body,
+            params
+        }
+    }
+
+}
