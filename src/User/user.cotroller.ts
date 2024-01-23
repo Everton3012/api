578ec6f324c63215cd3e757daaff3ca34163b420
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-01-23 17:53:01 -0300

    validações e DTos adicionados

--- src/User/user.cotroller.ts
+++ src/User/user.cotroller.ts
@@ -1,4 +1,7 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from "@nestjs/common";
+import { CreateUserDTO } from "./dto/create-user.dto";
+import { UpdateUserDTO } from "./dto/update-put-user.dto";
+import { UpdatePatchUserDTO } from "./dto/update-patch-user.dto";
 
 @Controller("users")
 export class UserController{
@@ -14,24 +17,24 @@ export class UserController{
     }
 
     @Post()
-    async create(@Body() body) {
-        return {body};
+    async create(@Body() {email, name, password}: CreateUserDTO) {
+        return {email, name, password};
     }
 
     @Put(":id")
-    async update(@Body() body,@Param() params){
+    async update(@Body() {email, name, password}: UpdateUserDTO,@Param() params){
         return {
             method: "put",
-            body,
+            email, name, password,
             params
         }
     }
 
     @Patch(":id")
-    async updatePartial(@Body() body, @Param() params){
+    async updatePartial(@Body() {email, name, password}: UpdatePatchUserDTO, @Param() params){
         return {
             method: "patch",
-            body,
+            email, name, password,
             params
         }
     }
